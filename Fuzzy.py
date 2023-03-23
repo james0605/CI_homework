d0f8592460e00aef526cf1d9bd1b4fa2a31e6bf4
james0605 <76697698+james0605@users.noreply.github.com>
2023-03-23 17:34:02 +0800

    Update Fuzzy.py
    寫Fuzzy.py的大致架構

---
+++ Fuzzy.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class Fuzzy(object):
+    def __init__(self) -> None:
+
+        pass
+
+    def MembershipFun(dist):
+        
+        pass
+    
+    def is_RightTurn():
+        pass
+
+    def is_Straight():
+        pass
+
+    def is_LeftTurn():
+        pass
